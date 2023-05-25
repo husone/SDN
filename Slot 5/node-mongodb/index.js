94fb5cd3a03f0a28e6bd4f7688da0ffe945d639c
husone <sonhvnde160056@fpt.edu.vn>
2023-05-25 08:30:11 +0700

    slot5 part2

--- Slot 5/node-mongodb/index.js
+++ Slot 5/node-mongodb/index.js
@@ -5,32 +5,28 @@ const dboper = require("./operations");
 const url = "mongodb://127.0.0.1:27017";
 const dbName = "conFursion";
 
-MongoClient.connect(url, (err, client) => {
-    assert.equal(err, null);
+MongoClient.connect(url).then((client) => {
 
     console.log("Connected correctly to server");
-
     const db = client.db(dbName);
-    const collection = db.collection("dishes");
-    dboper.insertDocument(db, { name: "Vadonut", description: "Test" },
-        "dishes", (result) => {
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test" },"dishes")
+        .then((result) => {
             console.log("Insert Document:\n", result.ops);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Documents:\n", docs);
+            return dboper.updateDocument(db, { name: "Vadonut" },
+                { description: "Updated Test" }, "dishes");
+        })
+        .then((result) => {
+            console.log("Updated Document:\n", result.result);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Updated Documents:\n", docs);
+            return db.dropCollection("dishes");
+        })
+        .catch((err) => console.log(err));
 
-            dboper.findDocuments(db, "dishes", (docs) => {
-                console.log("Found Documents:\n", docs);    
-                
-                dboper.updateDocument(db, { name: "Vadonut" }, { description: "Updated Test" }, "dishes", (result) => {
-                    console.log("Updated Document:\n", result.result);
-
-                    dboper.findDocuments(db, "dishes", (docs) => {
-                        console.log("Found Updated Documents:\n", docs);
-                        
-                        db.dropCollection("dishes", (result) => {
-                            console.log("Dropped Collection: ", result);
-                            client.close();
-                        });
-                    });
-                });
-            });
-        });
 });
